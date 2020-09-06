44192bfc46ae6ff56753e40c50f9e9372248f98d
AFigiel <70859584+AFigiel@users.noreply.github.com>
2020-09-06 13:41:54 -0600

    updated
    updated

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Simple Twitch Bot
-<<<<<<< HEAD
+
  A simple python Twitch Bot, made for the Raspberry Pi, using https://pimylifeup.com/raspberry-pi-twitch-bot/ as a guide.
  
  Looking for assistance on how to add additional code/commands to bot. 
  
 If using bot, replace credentials in "config.py" with your own. 
 Run Bot: "python3 ./main.py" in command line under bot's directory.
-=======
+
  A simple python Twitch Bot
---
+++ config.py
@@ -0,0 +1,10 @@
+HOST = "irc.twitch.tv"              # This is Twitchs IRC server
+PORT = 6667                         # Twitchs IRC server listens on port 6767
+NICK = "Bot_Username"            # Twitch username your using for your bot
+PASS = "oauth:xxxxxxxxxxxxxxxxxxxxxx" # your Twitch OAuth token
+CHAN = "https://www.twitch.tv/xxxxxxxxxxx"                   # the channel you want the bot to join.
+RATE = (20/30) # messages per seccond
+BAN_PAT = [
+    r"swear",
+    r"some_phrase",
+]
